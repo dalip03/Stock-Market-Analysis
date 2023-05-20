1500a9aeadbb4fb2819b10953b18d1c595ca345c
dalip <dalipdivakar@gmail.com>
2023-05-20 12:25:18 +0530

    ML models

--- send stoke/app1.py
+++ send stoke/app1.py
@@ -7,6 +7,13 @@ from TopLoserStocks import topLoseData
 import numpy as np
 from News_yahooStocks import yahooNews
 
+
+
+from flask import Flask, render_template, request, send_from_directory
+import utils
+import train_models as tm
+import os
+import pandas as pd
 #Global variables
 
 app=Flask(__name__)
@@ -16,30 +23,102 @@ app=Flask(__name__)
 #user defined routes
 
 
+
+
+
 @app.route("/", methods=["GET" ,"POST" ])
+
 def home1():   
     
     gain, lose ,gainlosehead =  topLoseData()
     news = yahooNews()
-    return render_template('indexjs.html',gain=gain, lose= lose ,gainlosehead = gainlosehead, news = news)
+    # return render_template('indexjs.html',gain=gain, lose= lose ,gainlosehead = gainlosehead, news = news)
+    return render_template('home.html',gain=gain, lose= lose ,gainlosehead = gainlosehead, news = news)
 
 @app.route("/table", methods=["GET" ,"POST" ])
 def home2():   
     context,tablehead =  tabledata()
     return render_template('table.html',context=context,tablehead = tablehead)
 
-   
-    
-# @app.route("/prediction",methods=['GET','POST'])
-# def predict():
-#     output = pre(list(df['text']))
-#     df['ouput'] = output
-#     context = df
-#     return render_template('prediction.html',tables=context)
 
 
+# prediction MAchine learning part
+
+def perform_training(stock_name, df, models_list):
+    all_colors = {'SVR_linear': '#FF9EDD',
+                  'SVR_poly': '#FFFD7F',
+                  'SVR_rbf': '#FFA646',
+                  'linear_regression': '#CC2A1E',
+                  'random_forests': '#8F0099',
+                  'KNN': '#CCAB43',
+                  'elastic_net': '#CFAC43',
+                  'DT': '#85CC43',
+                  'LSTM_model': '#CC7674'}
+
+    print(df.head())
+    dates, prices, ml_models_outputs, prediction_date, test_price = tm.train_predict_plot(stock_name, df, models_list)
+    origdates = dates
+    if len(dates) > 20:
+        dates = dates[-20:]
+        prices = prices[-20:]
+
+    all_data = []
+    all_data.append((prices, 'false', 'Data', '#000000'))
+    for model_output in ml_models_outputs:
+        if len(origdates) > 20:
+            all_data.append(
+                (((ml_models_outputs[model_output])[0])[-20:], "true", model_output, all_colors[model_output]))
+        else:
+            all_data.append(
+                (((ml_models_outputs[model_output])[0]), "true", model_output, all_colors[model_output]))
+
+    all_prediction_data = []
+    all_test_evaluations = []
+    all_prediction_data.append(("Original", test_price))
+    for model_output in ml_models_outputs:
+        all_prediction_data.append((model_output, (ml_models_outputs[model_output])[1]))
+        all_test_evaluations.append((model_output, (ml_models_outputs[model_output])[2]))
+
+    return all_prediction_data, all_prediction_data, prediction_date, dates, all_data, all_data, all_test_evaluations
+
+all_files = utils.read_all_stock_files('individual_stocks_5yr')
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+
+
+
+@app.route('/prediction')
+# ‘/’ URL is bound with hello_world() function.
+def landing_function():
+    # all_files = utils.read_all_stock_files('individual_stocks_5yr')
+    # df = all_files['A']
+    # # df = pd.read_csv('GOOG_30_days.csv')
+    # all_prediction_data, all_prediction_data, prediction_date, dates, all_data, all_data = perform_training('A', df, ['SVR_linear'])
+    stock_files = list(all_files.keys())
+
+    return render_template('prediction.html',show_results="false", stocklen=len(stock_files), stock_files=stock_files, len2=len([]),
+                           all_prediction_data=[],
+                           prediction_date="", dates=[], all_data=[], len=len([]))
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    stock_file_name = request.form['stockfile']
+    ml_algoritms = request.form.getlist('mlalgos')
+
+    # all_files = utils.read_all_stock_files('individual_stocks_5yr')
+    df = all_files[str(stock_file_name)]
+    # df = pd.read_csv('GOOG_30_days.csv')
+    all_prediction_data, all_prediction_data, prediction_date, dates, all_data, all_data, all_test_evaluations = perform_training(str(stock_file_name), df, ml_algoritms)
+    stock_files = list(all_files.keys())
+
+    return render_template('prediction.html',all_test_evaluations=all_test_evaluations, show_results="true", stocklen=len(stock_files), stock_files=stock_files,
+                           len2=len(all_prediction_data),
+                           all_prediction_data=all_prediction_data,
+                           prediction_date=prediction_date, dates=dates, all_data=all_data, len=len(all_data))
 
 if __name__ =='__main__':
-    app.run(debug=True      )       
+    app.run(host='0.0.0.0',port=8080 ,debug=True)       
 
 
--- send stoke/test.py
+++ send stoke/test.py
@@ -1,7 +1,15 @@
 from yahoo_fin.stock_info import *
 import yahoo_fin.stock_info as si
 
-top_gainers = 	si.get_day_gainers(3)
-stockcode = list(top_gainers['Symbol'])
 
-print(stockcode)
+def tabledata():
+    # Stocklist = ['TSLA','AAPL','BAC','MARA','AMZN','NVDA','NIO','RIOT','GOOGL','INFY','PLUG','APE','SOFI','META','MSFT','SNAP','USB','NYCB','DNA','UBER','AFRM','PYPL','PARA','MS','DELL','HPQ','HPE','DIS']
+    Stocklist = ['TSLA','AAPL','BAC']
+    # temp = si.get_quote_data('TSLA')
+    # stock_data = []
+    for i in Stocklist:
+        
+        temp = (si.get_quote_data(i))
+        print(temp)
+
+tabledata()
